ace7fd2084b1db5007f86420761b63b29c26560e
Axel Mennessier <axel.mennessier@ensiie.fr>
2022-11-10 17:23:54 +0100

    add test route

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,10 @@ def index():
 @app.route('/game')
 def game():
     return render_template("game.html")
+    
+@app.route('/test')
+def test():
+    return render_template("test.html")
 
 if __name__ == '__main__':
     app.run()
