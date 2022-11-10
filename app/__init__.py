165a36345121d3a838daab00ebfc066bebdd795b
Axel Mennessier <axel.mennessier@ensiie.fr>
2022-11-10 13:55:50 +0100

    add some route and html

--- app/__init__.py
+++ app/__init__.py
@@ -5,5 +5,17 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/add')
+def add():
+    return render_template("add.html")
+    
+@app.route('/game')
+def game():
+    return render_template("game.html")
+
+@app.route('/main')
+def main():
+    return render_template("main.html")
+
 if __name__ == '__main__':
     app.run()
