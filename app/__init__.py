ea444d22f3d01e3c736db7e4817d3e09fa138b95
Axel Mennessier <axel.mennessier@ensiie.fr>
2022-11-10 19:33:11 +0100

    ?

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ def game():
     except:
         return redirect(url_for('ask_name'))
 
-@app.route('/game/naasmke', methods = ['POST', 'GET'])
+@app.route('/game/ask_name', methods = ['POST', 'GET'])
 def ask_name():
     return render_template("game.html")
 
