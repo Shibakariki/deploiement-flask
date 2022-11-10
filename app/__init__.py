6a9c3b543f48bd9406ad890103cc212634a1ee14
Axel Mennessier <axel.mennessier@ensiie.fr>
2022-11-10 21:49:39 +0100

    r

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,7 @@ def game():
             jetons = redis_client.get(name)
         except:
             jetons = "100"
-            redis_client.set(name,jetons)
+            redis_client.set(name,"100")
             return redis_client.get(name)
         resp = make_response(render_template("game.html"))
         resp.set_cookie('ckitonbjt-v2',jetons)
