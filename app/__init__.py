a4d03b575a37f3bad29e0970e235da4821a98ee5
Axel Mennessier <axel.mennessier@ensiie.fr>
2022-11-10 23:34:14 +0100

    int

--- app/__init__.py
+++ app/__init__.py
@@ -22,7 +22,7 @@ def addJetons():
         try:
             redis_client.set(name,redis_client.get(name)+jetons)
         except:
-            var = str(type(redis_client.get(name)))
+            var = str(int(redis_client.get(name)))
             return var
         try:
             redis_client.rpush("add"+name,jetons)
